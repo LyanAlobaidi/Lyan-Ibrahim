78e1d5b150ae142216939e9ae664e08f90aec36c
LyanAlobaidi <s_ghazey@yahoo.com>
2025-05-27 22:08:20 +0400

    Add files via upload

---
+++ facial_email_finaala_try.py
@@ -0,0 +1,246 @@
+#! /usr/bin/python
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import imutils
+import pickle
+import time
+import cv2
+import requests
+import smtplib
+import pyttsx3
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import pyttsx3
+from gpiozero import Button
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+#Initialize 'currentname' to trigger only when a new person is identified.
+
+def send_message(name, img_name):
+    global stoop
+    stoop=1
+    global engine
+    engine = pyttsx3.init()
+    
+    username='amanymahdy92@gmail.com'
+    password='vljqpiocgymeshda'
+    # the email objects
+    subject= "Permission"
+    recipent='amanymahdy92@gmail.com'
+    content=name+" arrived to DEWA at " + time.ctime()
+    emailData = MIMEMultipart()
+    emailData['Subject'] = subject
+    emailData['To'] = recipent
+    emailData['From'] = username
+    
+            #Attach our text data
+    emailData.attach(MIMEText(content))
+
+    imageData = MIMEImage(open(img_name, 'rb').read(), 'jpg')
+    imageData.add_header('Content-Disposition', 'attachment; filename="image.jpg"')
+    emailData.attach(imageData)
+
+    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.ehlo()
+    s.login(username,password)
+# send the email
+    s.sendmail(username, recipent, emailData.as_string())
+    print(stoop)
+    rslt=s.quit()
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+def Camera():
+    global stoop
+    global engine
+    engine = pyttsx3.init()
+    
+    currentname = "unknown"
+    #Determine faces from encodings.pickle file model created from train_model.py
+    encodingsP = "encodings.pickle"
+    #use this xml file
+    cascade = "haarcascade_frontalface_default.xml"
+    print("[INFO] loading encodings + face detector...")
+    data = pickle.loads(open(encodingsP, "rb").read())
+    detector = cv2.CascadeClassifier(cascade)
+
+    # initialize the video stream and allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    # vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)
+
+    # start the FPS counter
+    fps = FPS().start()
+
+    stoop=0
+    # loop over frames from the video file stream
+    while True:
+    
+        frame = vs.read()
+        frame = imutils.resize(frame, width=500)
+    
+    # convert the input frame from (1) BGR to grayscale (for face
+    # detection) and (2) from BGR to RGB (for face recognition)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # detect faces in the grayscale frame
+        rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+            minNeighbors=5, minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # OpenCV returns bounding box coordinates in (x, y, w, h) order
+    # but we need them in (top, right, bottom, left) order, so we
+    # need to do a bit of reordering
+        boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+    # compute the facial embeddings for each face bounding box
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
+    
+    # loop over the facial embeddings
+        for encoding in encodings:
+        # attempt to match each face in the input image to our known
+        # encodings
+            matches = face_recognition.compare_faces(data["encodings"],
+                encoding)
+            name = "Unknown"
+
+        # check to see if we have found a match
+            if True in matches:
+            # find the indexes of all matched faces then initialize a
+            # dictionary to count the total number of times each face
+            # was matched
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+
+            # loop over the matched indexes and maintain a count for
+            # each recognized face face
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+
+            # determine the recognized face with the largest number
+            # of votes (note: in the event of an unlikely tie Python
+            # will select first entry in the dictionary)
+                name = max(counts, key=counts.get)
+            
+            #If someone in your dataset is identified, print their name on the screen
+                if currentname != name:
+                    currentname = name
+                    print(currentname)
+                #Take a picture to send in the email
+                    img_name = "image.jpg"
+                    cv2.imwrite(img_name, frame)
+                    print('Taking a picture.')
+                    engine.say('Hello'+name)
+                    engine.runAndWait()
+                    request = send_message(name,img_name)
+                
+                
+        # update the list of names
+            names.append(name)
+
+    # loop over the recognized faces
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+        # draw the predicted face name on the image - color is in BGR
+            cv2.rectangle(frame, (left, top), (right, bottom),
+                (0, 255, 225), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                .8, (0, 255, 255), 2)
+    # display the image to our screen
+        cv2.imshow("Facial Recognition is Running", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    
+        if stoop==1:
+            print(5)
+            break
+    # update the FPS counter
+        fps.update()
+    
+# stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+ 
+def button_callback(channel):
+    print("look at the camera")
+    Camera()
+
+
+ 
+ # code
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+Ena, In1,In2 =9,16,12
+EnaL, In1L,In2L =15,27,22
+Tri, Echo= 17,14
+GPIO.setup(Ena, GPIO.OUT)
+GPIO.setup(In1, GPIO.OUT)
+GPIO.setup(In2, GPIO.OUT)
+GPIO.setup(EnaL, GPIO.OUT)
+GPIO.setup(In1L, GPIO.OUT)
+GPIO.setup(In2L, GPIO.OUT)
+
+GPIO.setup(Tri, GPIO.OUT)
+GPIO.setup(Echo, GPIO.IN)
+
+pwm=GPIO.PWM(Ena,100)
+pwm.start(0)
+pwmL=GPIO.PWM(EnaL,100)
+pwmL.start(0)
+
+while True:
+    
+    GPIO.output(Tri,False)
+    print("waiting for sensor to settle")
+    time.sleep(0.2)
+    GPIO.output(Tri,True)
+    time.sleep(0.00001)
+    GPIO.output(Tri,False)
+    while GPIO.input(Echo)==0:
+        pulse_start=time.time()
+    while GPIO.input(Echo)==1:
+        pulse_end=time.time()
+    pulse_duration=pulse_end-pulse_start
+    distance=pulse_duration*17150
+    distance=round(distance,2)
+    print("distance:",distance,"cm")
+    time.sleep(2)
+    
+    if (distance>100):
+        GPIO.output(In1,GPIO.LOW)
+        GPIO.output(In2,GPIO.HIGH)  
+        pwm.ChangeDutyCycle(100)
+        
+        GPIO.output(In1L,GPIO.LOW)
+        GPIO.output(In2L,GPIO.HIGH)  
+        pwmL.ChangeDutyCycle(100)
+        time.sleep(2)
+    
+    elif (distance <=100):
+     
+       pwm.ChangeDutyCycle(0)
+       pwmL.ChangeDutyCycle(0)
+       time.sleep(2)
+       
+       GPIO.setwarnings(False) # Ignore warning for now
+       GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+       GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+       GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
+       message = input("Press enter to take attendance\n\n") # Run until someone presses enter
+       GPIO.cleanup() # Clean up
